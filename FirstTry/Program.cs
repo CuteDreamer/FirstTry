c1265b24037ae27b286209f0db2ea009ace61f39
Artem Karp <moment.of.peaces@gmail.com>
2024-05-29 21:38:22 +0300

    Добавьте файлы проекта.

---
+++ FirstTry/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите размер: ");
+int size = int.Parse(Console.ReadLine());
+Console.WriteLine("\n");
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("  ");
+    }
+    Console.Write("***\n");
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("  ");
+    }
+    Console.Write("  *\n");
+}
+
